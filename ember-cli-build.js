13b09e4937d027cb9190256f3c290a2f41a0f516
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2015-11-18 11:22:55 -0500

    trying out list groups

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -22,8 +22,10 @@ module.exports = function(defaults) {
 
   app.import('bower_components/ember-localstorage-adapter/localstorage_adapter.js');
   app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css.map');
 
-  app.import('vendor/bootstrap.lumen.min.css');
+  // app.import('vendor/bootstrap.lumen.min.css');
   // app.import('bower_components/bootstrap/dist/css/bootstrap.css');
   // app.import('bower_components/bootstrap/dist/css/bootstrap.css.map');
 
